bc9b989cd3f56c46d390e767faedec31285ce318
natbkgift <40134401+natbkgift@users.noreply.github.com>
2025-12-28 15:00:15 +0700

    config: set Astro site to flowbiz.cloud

--- web/astro/astro.config.mjs
+++ web/astro/astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://flowbiz.co.th',
+  site: 'https://flowbiz.cloud',
   compressHTML: true,
   build: {
     inlineStylesheets: 'auto'
